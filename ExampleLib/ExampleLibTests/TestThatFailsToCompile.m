4c1e24f82677dec7e867025ae68c4af4448a2e84
Noah Gilmore <noah.gilmore@autodesk.com>
2020-06-24 11:35:07 -0700

    Whoops, make it compile under xcode 11

--- ExampleLib/ExampleLibTests/TestThatFailsToCompile.m
+++ ExampleLib/ExampleLibTests/TestThatFailsToCompile.m
@@ -16,7 +16,7 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssertEqual([@"a" isEqualToString:@"a"]);
+    XCTAssertTrue([@"a" isEqualToString:@"a"]);
 }
 
 @end
